d774e41b8aa1781e63e425f66f316514a6630828
zchronis <chrono009@gmail.com>
2023-10-21 15:56:40 -0700

    created migrations

---
+++ database/migrations/2023_10_21_223354_create_apods_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apods', function (Blueprint $table) {
+            $table->id('apod_id');
+            $table->text('copyright');
+            $table->date('photo_date');
+            $table->text('explanation');
+            $table->text('hdurl');
+            $table->text('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apods');
+    }
+};
